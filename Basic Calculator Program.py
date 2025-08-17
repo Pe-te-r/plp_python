aaea6723c089e8aad700f413eae3753a8e4214f1
Pe-te-r <mburupeter8526@gmail.com>
2025-08-17 04:14:56 +0300

    working sample

--- Basic Calculator Program.py
+++ Basic Calculator Program.py
@@ -3,9 +3,8 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter the operation (+, -, *, /): ")
 
-# Perform calculation based on operation
 result = 0
-if operation == '+':
+if operation >= '+':
     result = num1 + num2
     print(f"{num1} {operation} {num2} = {result}")
 elif operation == '-':
